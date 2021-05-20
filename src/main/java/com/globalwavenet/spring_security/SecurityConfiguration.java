6cbac94ad7d9604f2918dcfdc8c6c3c022bf7340
hpanushan <anushan@globalwavenet.com>
2021-05-20 23:53:40 +0530

    Changed authntication method to basic auth

--- src/main/java/com/globalwavenet/spring_security/SecurityConfiguration.java
+++ src/main/java/com/globalwavenet/spring_security/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN","USER")
                 .antMatchers("/").permitAll()
-                .and().formLogin();
+                .and().httpBasic();
     }
 
     @Bean
