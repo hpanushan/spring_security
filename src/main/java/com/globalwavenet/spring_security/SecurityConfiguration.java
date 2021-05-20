83c3a87e688294e6a7b4610a0ed0c50fb3e14de3
hpanushan <anushan@globalwavenet.com>
2021-05-20 20:19:08 +0530

    H2 database integration with populating default schema and users for authentication and authorization

--- src/main/java/com/globalwavenet/spring_security/SecurityConfiguration.java
+++ src/main/java/com/globalwavenet/spring_security/SecurityConfiguration.java
@@ -1,27 +1,39 @@
 package com.globalwavenet.spring_security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     // For authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("anushan")
-                .password("anushan.1")
-                .roles("ADMIN")
-                .and()
-                .withUser("nimesh")
-                .password("nimesh.1")
-                .roles("USER");
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser(
+                        User.withUsername("anushan")
+                        .password("anushan.1")
+                        .roles("ADMIN")
+                )
+                .withUser(
+                        User.withUsername("nimesh")
+                        .password("nimesh.1")
+                        .roles("USER")
+                );
     }
 
     // For authorization
