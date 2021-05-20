26e23cad3ec8b0e926ecf1b822ee8d5464aab18c
hpanushan <anushan@globalwavenet.com>
2021-05-20 17:01:00 +0530

    Application authentication with custom authentication manager (In memory)

---
+++ src/main/java/com/globalwavenet/spring_security/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.globalwavenet.spring_security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("anushan")
+                .password("anushan.1")
+                .roles("ADMIN")
+                .and()
+                .withUser("nimesh")
+                .password("nimesh.1")
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
