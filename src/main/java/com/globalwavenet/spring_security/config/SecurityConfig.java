b137d64142a22e4a01f7fc741096a4015c189310
hpanushan <anushan@globalwavenet.com>
2021-05-21 21:53:33 +0530

    * Added JWT to authorize endpoints

--- src/main/java/com/globalwavenet/spring_security/config/SecurityConfig.java
+++ src/main/java/com/globalwavenet/spring_security/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.globalwavenet.spring_security.config;
 
+import com.globalwavenet.spring_security.filter.JwtFilter;
 import com.globalwavenet.spring_security.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -15,9 +21,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
+    // For authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService);
+    }
+
+    // For authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
     }
 
     @Bean
--- src/main/java/com/globalwavenet/spring_security/controller/UserController.java
+++ src/main/java/com/globalwavenet/spring_security/controller/UserController.java
@@ -1,13 +1,39 @@
 package com.globalwavenet.spring_security.controller;
 
+import com.globalwavenet.spring_security.entity.AuthRequest;
+import com.globalwavenet.spring_security.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @GetMapping("/")
     public String hello(){
         return "<h1>Hello World<h1>";
+    }
+
+    @PostMapping("/authenticate")
+    public String authenticate(@RequestBody AuthRequest authRequest) throws Exception{
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword()));
+        }catch (Exception e){
+            throw new Exception("invalid username or password");
+        }
+
+        // Generate token
+        return jwtUtil.generateToken(authRequest.getUserName());
     }
 }
